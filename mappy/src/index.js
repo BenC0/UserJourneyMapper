d6b96095ef1304c6ddea2f1519efcc31d0b905aa
Benji Cohen <135N@jdwilliamsgroup.com>
2019-12-20 09:05:54 +0000

    TDOOODODODODOODODOD

--- mappy/src/index.js
+++ mappy/src/index.js
@@ -14,6 +14,7 @@ get_data_from_csv(csvPath).then(data => {
 /* TODO: 
 	1.	Using the real data causes the whole thing to crash. There are so many rows we need
 		a better way to manage it. (Could be a memory)
+		- Maybe the data needs to be preformatted. Preformatting the data through Python would reduce the amount of shit the app needs to do. There would be a fuck ton less data because it's been smooshed (It's 9am the morning after the work party, doing real thinking and using actual terminology ain't happening...) 
 	2.	Wouldn't it be cool if we could upload files directly? - YES
 	3.	Different types of graphs would also be cool but let's not think about it for now.
 	4.	Need to work out how I can distribute this to the rest of the team
