42f7df9e4f60b421a9e7472b6ff7d958d4a4834f
Benji Cohen <135N@jdwilliamsgroup.com>
2020-01-06 17:55:25 +0000

    Added solution to TODO - needs building and testing

--- mappy/src/index.js
+++ mappy/src/index.js
@@ -15,6 +15,12 @@ get_data_from_csv(csvPath, data => {
 	1.	Using the real data causes the whole thing to crash. There are so many rows we need
 		a better way to manage it. (Could be a memory)
 		- Use Canvas instead of SVG. Apparently Canvas is significantly more efficient in terms of performance.
+		- Figured it out!
+			- Break it down into individual steps of the path and only render the segments that are needed
+			- so on the root it'll load the landing pages and display them in the sunburst (is it a pie chart at this point? doesn't matter right now...) and then when clicking page X it will load the subsequent steps of page X (a, b, c & d). After clicking one of those steps (a), it'll then load the subsequent steps of page a (e, f, g), etc.
+			- This reduces the number of rendered elements significantly (unusued elements can be cleared in between rendering steps)..
+			- Might also be able to stick with SVG, but this could be done with Canvas as well.
+			
 	2.	Wouldn't it be cool if we could upload files directly? - YES
 	3.	Different types of graphs would also be cool but let's not think about it for now.
 	4.	Need to work out how I can distribute this to the rest of the team
